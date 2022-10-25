8a2a48480168e7ccbecb119f9ae57cd23cb62b46
root <michaelosita158@gmail.com>
2022-10-25 10:05:27 -0700

    c-codes

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,19 +1,26 @@
 #include "main.h"
 
 /**
- * puts2 - skips 2
- * @str: string
+ * puts_half - prints half of a string.
+ * @str: param
  */
-void puts2(char *str)
+void puts_half(char *str)
 {
-	int i;
+	int count = 0, i;
 
-	i = 0;
-	while (str[i] != '\0')
+	while (count >= 0)
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-		i++;
+		if (str[count] == '\0')
+			break;
+		count++;
 	}
+
+	if (count % 2 == 1)
+		i = count / 2;
+	else
+		i = (count - 1) / 2;
+
+	for (i++; i < count; i++)
+		_putchar(str[i]);
 	_putchar('\n');
 }
